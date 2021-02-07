d65876ef29f50c9187327167160190738348d1c9
Alexandre <>
2021-02-07 15:29:26 -0300

    initial commit of project structure

---
+++ src/test/java/com/axtech/br/calculatorapi/CalculatorapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.axtech.br.calculatorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalculatorapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
